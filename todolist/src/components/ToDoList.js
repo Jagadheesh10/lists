7238083df2bfcb1b7a4351a108518b87cdf332b7
Jagadheesh10 <116737938+Jagadheesh10@users.noreply.github.com>
2023-02-15 21:08:29 +0530

    git push

---
+++ todolist/src/components/ToDoList.js
@@ -0,0 +1,311 @@
+import React, { useRef, useState } from "react";
+
+// ant design imports
+import { PlusOutlined, CheckSquareOutlined } from "@ant-design/icons";
+import { EditableProTable } from "@ant-design/pro-components";
+import { Button, Space, Tag, Typography, Input, Layout, ConfigProvider, Popconfirm, Form } from "antd";
+import enUS from "antd/locale/en_US";
+
+// importing components
+import TagList from "./TagList";
+
+//Importing Default Data
+import { defaultData } from "../data"; 
+
+const { Title } = Typography;
+const { Content, Footer } = Layout;
+
+const ToDoList = () => {
+
+  // Column Structure Definition for ProTable 
+  const columns = [
+    {
+      dataIndex: "index",
+      valueType: "indexBorder",
+      width: 48,
+      responsive: ['xxl', 'xl', 'lg', 'md'],
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      search: true,
+      formItemProps: {
+        rules: [
+          {
+            required: true,
+            message: "This item is required",
+          },
+          {
+            max: 100,
+            whitespace: true,
+            message: "The maximum character limit is 1000",
+          },
+        ],
+      },
+      sorter: (a, b) => a.title.localeCompare(b.title),
+      responsive: ['xxl', 'xl', 'lg', 'md', 'sm', 'xs'],
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      search: true,
+      formItemProps: {
+        rules: [
+          {
+            required: true,
+            message: "This item is required",
+          },
+          {
+            max: 100,
+            whitespace: true,
+            message: "The maximum character limit is 1000",
+          },
+        ],
+      },
+      sorter: (a, b) => a.description.localeCompare(b.description),
+      responsive: ['xxl', 'xl', 'lg', 'md', 'sm'],
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      filters: true,
+      onFilter: true,
+      valueType: "select",
+      search: true,
+      formItemProps: {
+        rules: [
+          {
+            required: true,
+            message: "This item is required",
+          },
+        ],
+      },
+      valueEnum: {
+        open: {
+          text: "Open",
+          status: "Default",
+        },
+        overdue: {
+          text: "Overdue",
+          status: "Error",
+        },
+        done: {
+          text: "Done",
+          status: "Success",
+        },
+        working: {
+          text: "Working",
+          status: "Processing",
+        },
+      },
+      responsive: ['xxl', 'xl', 'lg', 'md', 'sm', 'xs'],
+    },
+    {
+      title: "Tags",
+      dataIndex: "tag",
+      filters: true,
+      filterSearch: true,
+      search: true,
+      width: "20%",
+      renderFormItem: (_, { isEditable }) => {
+        return isEditable ? <TagList /> : <Input />;
+      },
+      render: (_, row) =>
+        row.tag &&
+        row.tag.map((item) => <Tag key={item.key}>{item.label}</Tag>),
+      responsive: ['xxl', 'xl', 'lg'],
+    },
+    {
+      title: "Creation time",
+      dataIndex: "timestamp",
+      valueType: "date",
+      sorter: (a, b) => new Date(a.timestamp) - new Date(b.timestamp),
+      editable: false,
+      responsive: ['xxl', 'xl', 'lg', 'md'],
+    },
+    {
+      title: "Due Date",
+      dataIndex: "dueDate",
+      valueType: "date",
+      search: true,
+      sorter: (a, b) => new Date(a.dueDate) - new Date(b.dueDate),
+      formItemProps: {
+        rules: [{
+          validator: (values) => {
+            const { field } = values
+            const current = editForm.getFieldValue(`${field.split(".")[0]}`) || {}
+            const currentDate = new Date(new Date().toDateString())
+            const dueDate = new Date(current.dueDate.toDate().toDateString())
+            const timestamp = new Date(current.timestamp.toDate().toDateString())
+            if( dueDate.getTime() < currentDate.getTime() || dueDate.getTime() < timestamp.getTime())
+              return Promise.reject(new Error("Due Date cannot be before today's date or creation date" ));
+            return Promise.resolve();
+          },
+        }]
+      },
+      responsive: ['xxl', 'xl', 'lg', 'md', 'sm', 'xs'],
+    },
+    {
+      title: "Operation",
+      valueType: "option",
+      key: "option",
+      render: (text, record, _, action) => [
+        <a
+          key="editable"
+          onClick={() => {
+            action?.startEditable?.(record.id);
+          }}
+        >
+          <Button type="link" size="small">
+            Edit
+          </Button>
+        </a>,
+        <Popconfirm
+          title="Sure to delete?"
+          onConfirm={() => handleDelete(record.id)}
+        >
+          <Button type="link" size="small">
+            Delete
+          </Button>
+        </Popconfirm>,
+      ],
+      responsive: ['xxl', 'xl', 'lg', 'md', 'sm'],
+    },
+  ];
+
+  // Hooks for Pro Table 
+  const actionRef = useRef();
+  const [dataSource, setDataSource] = useState(defaultData);
+  const [editableKeys, setEditableRowKeys] = useState([]);
+  const [editForm] = Form.useForm()
+
+  // To delete a task
+  const handleDelete = (id) => {
+    const newData = dataSource.filter((item) => item.id !== id);
+    setDataSource(newData);
+  };
+
+  const waitTime = (time = 100) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, time);
+    });
+  };
+
+  return (
+
+    // Used to Change Component Languag labels to English
+    <ConfigProvider locale={enUS}> 
+      <Layout>
+        <Space
+          style={{ width: "100%", justifyContent: "center" }}
+        >
+          <Title>
+             TO DO LIST
+          </Title>
+        </Space>
+
+        <Content style={{ padding: "0 2%" }}>
+          <EditableProTable
+            columns={columns}
+            actionRef={actionRef}
+            cardBordered={true}
+            recordCreatorProps={false}
+            request={async (params = {}, sorter, filter) => {
+              const formParams = Object.entries(params).filter(
+                ([key, value]) => key !== "current" && key !== "pageSize"
+              );
+
+              return Promise.resolve({
+
+                // Code for Search function
+                data: defaultData.filter((item) => {
+                  if (formParams.length === 0) {
+                    return true;
+                  }
+                  var result = true;
+                  var formObj = Object.fromEntries(formParams);
+                  for (const key in formObj) {
+                    if (
+                      key !== "tag" &&
+                      key !== "dueDate" &&
+                      key !== "timestamp"
+                    ) {
+                      result =
+                        result &
+                        item[key]
+                          .toLowerCase()
+                          .includes(formObj[key].toLowerCase());
+                    } else if (key === "dueDate" || key === "timestamp") {
+                      result =
+                        result &
+                        (new Date(item[key]).toISOString().slice(0, 10) ===
+                          formObj[key]);
+                    } else {
+                      result =
+                        result &
+                        (item[key].filter(
+                          (i) =>
+                            i.label.toLowerCase() === formObj[key].toLowerCase()
+                        ).length >
+                          0);
+                    }
+                  }
+                  return result;
+                }),
+                success: true,
+              });
+            }}
+            value={dataSource}
+            rowKey="id"
+            search={{
+              layout: "vertical",
+              labelWidth: "auto",
+            }}
+            options={{
+              setting: {
+                listsHeight: 400,
+              },
+            }}
+            pagination={{
+              pageSize: 5,
+            }}
+            dateFormatter="string"
+            headerTitle="Task List"
+            onChange={setDataSource}
+            // Used for Editing Row Data
+            editable={{
+              form: editForm,
+              type: "multiple",
+              editableKeys,
+              onSave: async (data) => {
+                await waitTime(1000);
+              },
+              onChange: setEditableRowKeys,
+            }}
+            toolBarRender={() => [
+              <Space>
+                <Button 
+                  type="primary"
+                  onClick={() => {
+                    actionRef.current?.addEditRecord?.({
+                      id: new Date().getTime().toString(),
+                      timestamp: new Date().toDateString(),
+                      status: "open",
+                    });
+                  }}
+                  icon={<PlusOutlined />}
+                >
+                  Add New Task
+                </Button>
+              </Space>,
+            ]}
+          />
+        </Content>
+        </Layout>
+    </ConfigProvider>
+  );
+};
+
+export default ToDoList;
---
+++ todolist/src/data.js
@@ -0,0 +1,30 @@
+export const defaultData = [
+  {
+    id: new Date('2023-02-15').getTime().toString(),
+    status: "working",
+    timestamp: new Date('2023-02-15').toDateString(),
+    title: "Blockchain",
+    description: "Api integration",
+    dueDate: new Date('2023-02-16').toDateString(),
+    tag: [{ key: 'new',label: 'Gpt' },{ key: 'new_0',label: 'remix' }],
+  },
+  {
+    id: new Date('2023-02-15').getTime().toString(),
+    status: "working",
+    timestamp: new Date('2023-02-15').toDateString(),
+    title: "AI/Ml",
+    description: "Flask",
+    dueDate: new Date('2023-02-16').toDateString(),
+    tag: [{ key: 'new',label: 'pytorch' },{ key: 'new_0',label: 'jupiter' }],
+  },
+  {
+    id: new Date('2023-02-15').getTime().toString(),
+    status: "working",
+    timestamp: new Date('2023-02-15').toDateString(),
+    title: "web based",
+    description: "Backend working",
+    dueDate: new Date('2023-02-16').toDateString(),
+    tag: [{ key: 'new',label: 'W3' },{ key: 'new_0',label: 'javatpoint' }],
+  },
+  
+  ]
